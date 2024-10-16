8f4f37333fa1d9c4d7a78269c4c373e864cabe2b
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-10-16 07:54:06 -0300

    inicializacao back
    efetuada a criacao base do back

---
+++ back/activityList-API/Controllers/ActivityController.cs
@@ -0,0 +1,39 @@
+﻿using activityList_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace activityList_API.Controllers;
+
+[ApiController]
+[Route("controller")]
+public class ActivityController : ControllerBase
+{
+    [HttpGet]
+    public Activity Get()
+    {
+        return new Activity();
+    } 
+       
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "meu endpoint";
+    } 
+    
+    [HttpPost]
+    public string Post()
+    {
+        return "meu endpoint";
+    }
+    
+    [HttpPut]
+    public string Put()
+    {
+        return "meu endpoint";
+    }
+    
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return "meu endpoint";
+    }
+}
---
+++ back/activityList-API/Models/Activity.cs
@@ -0,0 +1,9 @@
+﻿namespace activityList_API.Models;
+
+public class Activity
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Priority { get; set; }
+}
